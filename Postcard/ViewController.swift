08c60155f4e0c9c4f88adb0ff4a056f6ba826c4d
Brad Duns <github@bradduns.com>
2015-01-31 13:20:07 -0700

    Added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,12 +28,14 @@ class ViewController: UIViewController {
 
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Code will Evaluate when we press the button
+        // Adding a Comment Here to test commits 
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
+        enterMessageTextField.resignFirstResponder(
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
